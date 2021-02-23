225ec99cc237697717bcd86341de2592449623cf
mikpearly <31659125+mikpearly@users.noreply.github.com>
2021-02-23 11:28:34 +0100

    color changes

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -40,8 +40,8 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+              '#f09b0d 0.5rem 0px 0px, #f09b0d -0.5rem 0px 0px',
+            backgroundColor: '#f09b0d',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
@@ -53,8 +53,8 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+              '#f09b0d 0.5rem 0px 0px, #f09b0d -0.5rem 0px 0px',
+            backgroundColor: '#f09b0d',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
