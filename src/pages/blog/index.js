a090c9bbd1f1b683353cdcbf29429ed2d1eba193
mikpearly <31659125+mikpearly@users.noreply.github.com>
2021-03-08 12:43:19 +0100

    change to primary color on full width image h2

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -16,8 +16,8 @@ export default class BlogIndexPage extends React.Component {
           <h1
             className="has-text-weight-bold is-size-1"
             style={{
-              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-              backgroundColor: '#f40',
+              boxShadow:  '#f09b0d 0.5rem 0px 0px, #f09b0d -0.5rem 0px 0px',
+              backgroundColor: '#f09b0d',
               color: 'white',
               padding: '1rem',
             }}
