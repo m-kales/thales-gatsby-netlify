fd0021dbebe62be1e5fb0e72c933f29cb99b13fc
mikpearly <31659125+mikpearly@users.noreply.github.com>
2021-03-08 12:51:28 +0100

    Updated site meta data: title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Thales Day',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Aiming to encapsulate and inspire our pursuit of human flourishing and our continued quest for shared principles that enable constructive dialogue and mutual understanding. ',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
