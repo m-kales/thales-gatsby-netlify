4364740d45be13ef8b41a0298a77a4766f140711
mikpearly <31659125+mikpearly@users.noreply.github.com>
2021-03-08 12:06:37 +0100

    Set brand colour on full width image h2 background

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -30,8 +30,8 @@ export const ProductPageTemplate = ({
       <h2
         className="has-text-weight-bold is-size-1"
         style={{
-          boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-          backgroundColor: '#f40',
+          boxShadow:  '#f09b0d 0.5rem 0px 0px, #f09b0d -0.5rem 0px 0px',
+          backgroundColor: '#f09b0d',
           color: 'white',
           padding: '1rem',
         }}
