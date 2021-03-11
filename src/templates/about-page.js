88cf8924b8bedbb5c948eb9a1e8ece56ac24ba15
mikpearly <31659125+mikpearly@users.noreply.github.com>
2021-03-11 12:18:55 +0100

    Center column is-6 layout on content

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -10,8 +10,8 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   return (
     <section className="section section--gradient">
       <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
+        <div className="columns is-centered">
+          <div className="column is-6">
             <div className="section">
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                 {title}
