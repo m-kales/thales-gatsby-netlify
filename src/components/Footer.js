60c0b56ddcbf36a7afe85d780845fa1c1b6f62f9
mikpearly <31659125+mikpearly@users.noreply.github.com>
2021-03-24 14:07:36 +0100

    Fix link content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,6 @@ import { Link } from 'gatsby'
 import logo from '../img/thalesday_logo_symbol-type_vert_colour_rgb-01.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
 
 const Footer = class extends React.Component {
   render() {
@@ -29,18 +28,13 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
                       <Link className="navbar-item" to="/products">
-                        Products
+                        Contribute
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
+                      <Link className="navbar-item" to="/words">
+                        Words of Wisdom
                       </Link>
                     </li>
                     <li>
@@ -61,7 +55,7 @@ const Footer = class extends React.Component {
                   <ul className="menu-list">
                     <li>
                       <Link className="navbar-item" to="/blog">
-                        Latest Stories
+                        Blog
                       </Link>
                     </li>
                     <li>
@@ -73,22 +67,14 @@ const Footer = class extends React.Component {
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
+                <a title="facebook" href="https://www.facebook.com/traditionofphilosophyandscience/">
                   <img
                     src={facebook}
                     alt="Facebook"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
+                <a title="instagram" href="https://www.instagram.com/thalesday/">
                   <img
                     src={instagram}
                     alt="Instagram"
