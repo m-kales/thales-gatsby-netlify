dae1937cd939d8e23a6c44745c1cad667f54e9f3
mikpearly <31659125+mikpearly@users.noreply.github.com>
2021-03-24 13:45:39 +0100

    Move around nav-items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -60,11 +60,11 @@ const Navbar = class extends React.Component {
             className={`navbar-menu has-background-black-ter ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
+            <Link className="navbar-item" to="/products">
+                Contribute
+              </Link>
               <Link className="navbar-item" to="/words">
                 Words of Wisdom
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Contribute
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
