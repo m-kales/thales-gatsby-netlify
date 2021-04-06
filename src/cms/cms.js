640e5a8139964379e8823303dd80f90531bc3611
mikpearly <31659125+mikpearly@users.noreply.github.com>
2021-04-06 09:57:48 +0200

    Fix about page preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -11,6 +11,6 @@ CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('words', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
