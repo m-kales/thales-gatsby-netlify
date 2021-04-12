28d85481cbf53afafbe12805e48ecbbf0990d17e
mikpearly <31659125+mikpearly@users.noreply.github.com>
2021-04-12 13:34:20 +0200

    Revert "Revert "Revert "Fix styling on products"""
    This reverts commit f5bc6e94786be045cad293ce5ff86e85a1d2053a.

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -3,20 +3,18 @@ import PropTypes from 'prop-types'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
 const Pricing = ({ data }) => (
-  <div className="tile is-ancestor">
+  <div className="columns">
     {data.map((price) => (
-      <div key={price.plan} className="tile is-third is-parent is-vertical">
-        <section className="section tile is-child block">
-          <div className="block product-img">
-            <PreviewCompatibleImage imageInfo={price.prodImg}/>
-          </div>
-          <h4 className="block has-text-centered has-text-weight-semibold">
+      <div key={price.plan} className="column">
+        <section className="section">
+        <PreviewCompatibleImage imageInfo={price.prodImg} />
+          <h4 className="has-text-centered has-text-weight-semibold">
             {price.plan}
           </h4>
-          <h2 className="block is-size-1 has-text-weight-bold has-text-primary has-text-centered">
+          <h2 className="is-size-1 has-text-weight-bold has-text-primary has-text-centered">
             ${price.price}
           </h2>
-          <p className="block has-text-weight-semibold">{price.description}</p>
+          <p className="has-text-weight-semibold">{price.description}</p>
           <ul>
             {price.items.map((item) => (
               <li key={item} className="is-size-5">
