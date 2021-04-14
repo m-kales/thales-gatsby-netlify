6cf326bc94f7be0c9f0332e5e4ceb34f7dba65d7
mikpearly <31659125+mikpearly@users.noreply.github.com>
2021-04-14 08:52:34 +0200

    Fix user agent blockquote style problem

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,6 +5,7 @@ import { Helmet } from 'react-helmet'
 import { graphql, Link } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
+import '../components/_extra.css'
 
 export const BlogPostTemplate = ({
   content,
