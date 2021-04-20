8f800f17efed7fa6865515880de66b98fa22c4de
mikpearly <31659125+mikpearly@users.noreply.github.com>
2021-04-20 10:58:19 +0200

    Delete words-page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,7 +2,6 @@ import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
-import WordsPagePreview from './preview-templates/WordsPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import InfoPagePreview from './preview-templates/InfoPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
@@ -11,6 +10,5 @@ CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('words', WordsPagePreview)
 CMS.registerPreviewTemplate('info', InfoPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
