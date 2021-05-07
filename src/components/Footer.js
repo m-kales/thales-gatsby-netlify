ab4ef02ec93f0ab680b0b3ecc4b417e641386eec
mikpearly <31659125+mikpearly@users.noreply.github.com>
2021-05-07 08:57:50 +0200

    Add anilink to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Link } from 'gatsby'
-
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
 import logo from '../img/thalesday_logo_vertical_rgb_gradient_no-bg.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
@@ -10,11 +9,18 @@ const Footer = class extends React.Component {
     return (
       <footer className="footer has-background-black-bis has-text-white-ter">
         <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Thales Day"
-            style={{ width: '14em', height: '10em' }}
-          />
+          <AniLink
+            cover 
+            direction="left" 
+            bg="#f09b0d"
+            to="/" 
+          >
+            <img
+              src={logo}
+              alt="Thales Day"
+              style={{ width: '6em' }}
+            />
+          </AniLink>
         </div>
         <div className="content has-text-centered has-text-white-ter">
           <div className="container has-text-white-ter">
@@ -23,14 +29,14 @@ const Footer = class extends React.Component {
                 <section className="menu">
                   <ul className="menu-list">
                     <li>
-                      <Link to="/" className="navbar-item">
+                      <AniLink cover direction="right" bg="#f09b0d" to="/" className="navbar-item">
                         Home
-                      </Link>
+                      </AniLink>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/info">
+                      <AniLink cover direction="left" bg="#f09b0d" className="navbar-item" to="/info">
                         Info
-                      </Link>
+                      </AniLink>
                     </li>
                     <li>
                       <a
@@ -49,27 +55,27 @@ const Footer = class extends React.Component {
                 <section>
                   <ul className="menu-list">
                     <li>
-                      <Link className="navbar-item" to="/blog">
+                      <AniLink cover direction="left" bg="#f09b0d" className="navbar-item" to="/blog">
                         Blog
-                      </Link>
+                      </AniLink>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/contact">
+                      <AniLink cover direction="left" bg="#f09b0d" className="navbar-item" to="/contact">
                         Contact
-                      </Link>
+                      </AniLink>
                     </li>
                   </ul>
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://www.facebook.com/traditionofphilosophyandscience/" target="_blank" rel="noreferrer noopener">
+                <a title="facebook" href="https://www.facebook.com/traditionofphilosophyandscience/">
                   <img
                     src={facebook}
                     alt="Facebook"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="instagram" href="https://www.instagram.com/thalesday/" target="_blank" rel="noreferrer noopener">
+                <a title="instagram" href="https://www.instagram.com/thalesday/">
                   <img
                     src={instagram}
                     alt="Instagram"
